53e44f859b63df1d2fe31293afcf67db6f41fa28
lyn-1234 <93703371+lyn-1234@users.noreply.github.com>
2024-02-15 18:28:36 +0200

    first commit

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { FormEventHandler, useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { addTodo } from "../../api";
+import { useRouter } from "../../node_modules/next/navigation";
+import Modal from "./Modal";
+import {v4 as uuidv4} from 'uuid';
+
+
+const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
+  const router = useRouter();
+  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await addTodo({
+      id:uuidv4(),
+      text: newTaskValue
+    })
+    setNewTaskValue('');
+    setModalOpen(false);
+    router.refresh();
+  }
+  
+  return (
+    <div>
+        <button onClick={() => setModalOpen(true)} className="btn btn-primary w-full">
+          Add new task<AiOutlinePlus size={18} className="ml-2"/></button>
+        <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+          <form onSubmit={handleSubmitNewTodo}>
+          <h3 className="font-bold text-lg">Add new task</h3>
+          <div className="modal-action">
+          <input
+          value={newTaskValue}
+          onChange={e => setNewTaskValue(e.target.value)}
+          type="text" 
+          placeholder="Type here"
+           className="input input-bordered w-full" />
+            <button type="submit" className="btn">Submit</button>
+            </div> 
+            </form>       
+        </Modal>
+    </div>
+  )
+}
+
+export default AddTask
---
+++ app/components/TodoList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ITask } from '../../types/tasks';
+
+import Task from "./Task";
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Tasks</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Check if tasks is defined before mapping over it */}
+          {tasks && tasks.map(task => (
+            <Task key={task.id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TodoList;
